eeb5ced9400985d66ff3dc57ebf017afd4b3d6cd
Jannaee Sick <jsick@mbp-jsic.medthink.local>
2019-07-14 00:41:11 -0400

    feat: adding axios call

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
   head() {
     return {
       title: 'Event Listing'
     }
   },
-  asyncData(context) {
-    return context.$axios
-      .get('http://localhost:3000/events')
-      .then((response) => {
-        return {
-          events: response.data
-        }
-      })
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then((response) => {
+      return {
+        events: response.data
+      }
+    })
+  },
+  components: {
+    EventCard
   }
 }
 </script>
